634ff68d7868be9901c479f4e59d341fd9d23890
laiyunlian <2076592250@qq.com>
2020-09-20 16:05:59 +0800

    vuex

---
+++ simple_vuex/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from "./components/productListOne";
+import ProductListTwo from "./components/productListTwo";
+export default {
+  name: 'App',
+    components: {ProductListTwo, ProductListOne},
+    data() {
+      return {
+
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  font-family: ubuntu;
+  color: #555;
+}
+</style>
---
+++ simple_vuex/src/components/productListOne.vue
@@ -0,0 +1,54 @@
+ <template>
+  <div id="product-list-two">
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for="product in saleProducts">
+        <span class="name">{{product.name}}</span>
+        <span class="price">${{product.price}}</span>
+      </li>
+    </ul>
+    <button @click="reducePrice(2)">商品降价</button>
+  </div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex'
+    import {mapActions} from 'vuex'
+    export default {
+        computed:{
+            // products() {
+            //     return this.$store.state.products
+            // },
+
+            // 单个的getters
+            // saleProducts() {
+            //     return this.$store.getters.saleProducts;
+            // },
+            // 多个的getters
+            ...mapGetters([
+                "saleProducts"
+            ])
+        },//使用...如果报错，执行 cnpm install babel-preset-stage-2
+        methods: {
+            // reducePrice(amount) {
+                // strict:true 时会报错
+                // this.$store.state.products.forEach(product =>{
+                //     product.price -= 1;
+                // })
+
+                // 调用的store mutations的方法
+                // this.$store.commit('reducePrice');
+
+                // 执行store actions的方法，异步获取（有调试记录）
+                // this.$store.dispatch('reducePrice',amount);
+            // },
+            ...mapActions([
+                "reducePrice"
+            ])
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
